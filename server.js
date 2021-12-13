f97fd50ec6939e6858c05d61b087ef1f6ebe72bc
mamutovdmytro <dmitriy.mamutov@gmail.com>
2021-12-14 01:57:48 +0200

    add cors

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const UserRouter = require('./api/User');
 const bodyParser = require('express').json;
 app.use(bodyParser());
 
+const cors = require("cors");
+app.use(cors());
+
 app.use('/user', UserRouter)
 
 app.listen(port, ()=>{
