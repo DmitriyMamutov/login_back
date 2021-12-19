50a77e97ba29259cab33a288f258efed1502090c
mamutovdmytro <dmitriy.mamutov@gmail.com>
2021-12-19 17:45:23 +0200

    add back

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI, {useNewUrlParser:true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("DB connected");
   })
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-require('./config/db');
+require("./config/db");
 
-const app = require('express')();
+const app = require("express")();
 const port = process.env.PORT || 3000;
 
-const UserRouter = require('./api/User');
+const UserRouter = require("./api/User");
 
-const bodyParser = require('express').json;
+const bodyParser = require("express").json;
 app.use(bodyParser());
 
 const cors = require("cors");
 app.use(cors());
 
-app.use('/user', UserRouter)
+app.use("/user", UserRouter);
 
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
